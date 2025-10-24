0aa3604f1e6103ccedd6bde842e406282ff3d743
matcoco <45208376+matcoco@users.noreply.github.com>
2025-10-24 23:09:54 +0200

    suppression aliment par defaut

--- js/config.js
+++ js/config.js
@@ -14,8 +14,4 @@ export const defaultActivities = [
 ];
 
 export const defaultFoods = {
-    'haricots-verts': { name: 'Haricots Verts', calories: 36, proteins: 1.7, carbs: 3.3, sugars: 1.1, fibers: 2.7, fats: 0.5 },
-    'comté': { name: 'Comté', calories: 410, proteins: 26, carbs: 0.5, sugars: 0.5, fibers: 0, fats: 34 },
-    // ... collez le reste de vos aliments par défaut ici ...
-    'raisin-vert': { name: 'Raisin vert', calories: 69, proteins: 0.7, carbs: 18.1, sugars: 16.3, fibers: 0.9, fats: 0.2 }
 };
